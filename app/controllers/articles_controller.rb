a94feebd56c0442bb88d2e4ae07d9889e74a97c1
Cici254 <thukucindy00@gmail.com>
2023-07-05 08:18:33 +0300

    complete assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,22 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    articles = Article.all.includes(:user).order(created_at: :desc)
+    articles = Article.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
-    article = Article.find(params[:id])
+    article = find_article
     render json: article
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def find_article
+    Article.find(params[:id])
   end
 
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
